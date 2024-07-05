5454989f50af20e02ec2b01efefd4265950a999e
jpadial15 <jorge.r.padial.doble@vanderbilt.edu>
2024-07-05 16:06:14 -0500

    added dataconfig file

---
+++ dataconfig.py
@@ -0,0 +1,46 @@
+import os
+
+
+HOME_DIR = '~/ALEXIS_pipeline'
+MAIN_DIR_NEW = '/lus/eagle/clone/g2/projects/ALEXIS'
+MAIN_DATA_DIR = ''
+
+#############################################
+JSOC_DL_DIR = "jsoc.download"
+
+NOTIFY_EMAIL_ADDR = 'jorge.r.padial.doble@vanderbilt.edu'
+
+############################################
+
+
+
+DATA_DIR_PRODUCTS = f'{MAIN_DIR_NEW}/data_products'
+
+DATA_DIR_GOES_FLUX = f'{MAIN_DIR_NEW}/goes_data_flux'
+
+DATA_DIR_GOES_SXI = f'{MAIN_DIR_NEW}/goes_data_sxi'
+
+DATA_DIR_AIA = f'{MAIN_DIR_NEW}/aia_data'
+
+DATA_DIR_HINODE = f'{MAIN_DIR_NEW}/hinode_data'
+
+DATA_DIR_HMI_HEK_AR = f'{MAIN_DIR_NEW}/hmi_hek_ar'
+
+DATA_DIR_FLARE_CANDIDATES = f'{MAIN_DIR_NEW}/flare_candidates'
+
+DATA_DIR_HMI_DRMS = f'{MAIN_DIR_NEW}/hmi_drms_ar'
+
+DATA_DIR_PLOTS = f'{MAIN_DIR_NEW}/PLOTS'
+
+DATA_DIR_IMG_DATA = f'{MAIN_DATA_DIR}/image_data'
+
+
+
+DATA_DIR_IMG_RUFFUS_OUTPUT = f'{DATA_DIR_IMG_DATA}/ruffus_output_files'
+
+#-----------------------
+
+
+
+RUFFUS_SQLITE_CHECKPOINT_DIR = f'{HOME_DIR}/ruffus_sqlite_checkpoints'
+
