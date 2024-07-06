5bf7cb44eae3c3887aa98bbfcb258db7d26f6d32
jpadial15 <jorge.r.padial.doble@vanderbilt.edu>
2024-07-06 15:04:04 -0500

    changed xrs_data_flux directory

--- dataconfig.py
+++ dataconfig.py
@@ -12,16 +12,8 @@ NOTIFY_EMAIL_ADDR = 'jorge.r.padial.doble@vanderbilt.edu'
 
 ############################################
 
-
+# paths that live in server
 DATA_DIR_PRODUCTS = f'{MAIN_DIR_NEW}/data_products'
-
-DATA_DIR_GOES_FLUX = f'{MAIN_DIR_NEW}/goes_data_flux'
-
-DATA_DIR_GOES_SXI = f'{MAIN_DIR_NEW}/goes_data_sxi'
-
-DATA_DIR_AIA = f'{MAIN_DIR_NEW}/aia_data'
-
-DATA_DIR_HINODE = f'{MAIN_DIR_NEW}/hinode_data'
 
 DATA_DIR_HMI_HEK_AR = f'{MAIN_DIR_NEW}/hmi_hek_ar'
 
@@ -33,13 +25,15 @@ DATA_DIR_PLOTS = f'{MAIN_DIR_NEW}/PLOTS'
 
 DATA_DIR_IMG_DATA = f'{MAIN_DATA_DIR}/image_data'
 
+# paths that live on harddrive
 
+DATA_DIR_GOES_FLUX = f'{MAIN_DATA_DIR}/xrs_data_flux'
+
+DATA_DIR_GOES_SXI = f'{MAIN_DIR_NEW}/goes_data_sxi'
 
 DATA_DIR_IMG_RUFFUS_OUTPUT = f'{DATA_DIR_IMG_DATA}/ruffus_output_files'
 
-#-----------------------
-
-
+DATA_DIR_AIA = f'{MAIN_DIR_NEW}/aia_data'
 
 RUFFUS_SQLITE_CHECKPOINT_DIR = f'{HOME_DIR}/ruffus_sqlite_checkpoints'
 
