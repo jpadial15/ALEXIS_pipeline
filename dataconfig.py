b9cb8e1cd825ceb9dbfe9b807d2673a65416aafe
jpadial15 <jorge.r.padial.doble@vanderbilt.edu>
2024-07-09 14:48:53 -0500

    hek_flares_directory created

--- dataconfig.py
+++ dataconfig.py
@@ -23,9 +23,11 @@ DATA_DIR_HMI_DRMS = f'{MAIN_DIR_NEW}/hmi_drms_ar'
 
 DATA_DIR_PLOTS = f'{MAIN_DIR_NEW}/PLOTS'
 
-DATA_DIR_IMG_DATA = f'{MAIN_DATA_DIR}/image_data'
+DATA_DIR_IMG_DATA = f'{MAIN_DATA_DIR}'
 
 # paths that live on harddrive
+
+DATA_DIR_HEK_FLARES = f'{MAIN_DATA_DIR}/hek_flares_data'
 
 DATA_DIR_GOES_FLUX = f'{MAIN_DATA_DIR}/xrs_data_flux'
 
@@ -33,7 +35,7 @@ DATA_DIR_GOES_SXI = f'{MAIN_DIR_NEW}/goes_data_sxi'
 
 DATA_DIR_IMG_RUFFUS_OUTPUT = f'{DATA_DIR_IMG_DATA}/ruffus_output_files'
 
-DATA_DIR_AIA = f'{MAIN_DIR_NEW}/aia_data'
+DATA_DIR_AIA = f'{MAIN_DATA_DIR}/aia_data'
 
 RUFFUS_SQLITE_CHECKPOINT_DIR = f'{HOME_DIR}/ruffus_sqlite_checkpoints'
 
