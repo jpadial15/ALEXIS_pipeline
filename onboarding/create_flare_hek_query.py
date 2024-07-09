d5bf01c5bc1d78020241921fe14403499f1e0002
jpadial15 <jorge.r.padial.doble@vanderbilt.edu>
2024-07-09 17:45:44 -0500

    sys path insert for modules in other directory

--- onboarding/create_flare_hek_query.py
+++ onboarding/create_flare_hek_query.py
@@ -1,8 +1,11 @@
+import sys  
+sys.path.insert(1, '..')
+sys.path.insert(2, '../modules/')
+
 import pickle
 import os
 import pandas as pd
 import numpy as np
-import pandas as pd
 from sunpy.time import TimeRange
 import sunpy.instr.goes
 import time
