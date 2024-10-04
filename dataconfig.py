c379c68a8dba33bf8baefcb4e70388483203be6c
jpadial15 <jorge.r.padial.doble@vanderbilt.edu>
2024-10-04 15:04:15 -0500

    changed image_dir to point to external hd directory

--- dataconfig.py
+++ dataconfig.py
@@ -15,16 +15,17 @@ NOTIFY_EMAIL_ADDR = 'jorge.r.padial.doble@vanderbilt.edu'
 ############################################
 
 # paths that live in server
-DATA_DIR_PRODUCTS = f'{MAIN_DIR_NEW}/data_products'
+DATA_DIR_PRODUCTS = f'{MAIN_DIR_NEW}/data_products' # data products has to live in MAIN_DATA_DIR # need to change # JRP 08/04/24 2:43 PM
 
 DATA_DIR_HMI_HEK_AR = f'{MAIN_DIR_NEW}/hmi_hek_ar'
 
-DATA_DIR_FLARE_CANDIDATES = f'{MAIN_DIR_NEW}/flare_candidates'
 
 DATA_DIR_PLOTS = f'{MAIN_DIR_NEW}/PLOTS'
 
 # paths that live on harddrive
-DATA_DIR_IMG_DATA = f'{MAIN_DATA_DIR}'
+DATA_DIR_FLARE_CANDIDATES = f'{MAIN_DATA_DIR}/flare_candidates'
+
+DATA_DIR_IMG_DATA = f'{MAIN_DATA_DIR}/image_data'
 
 DATA_DIR_HMI_DRMS = f'{MAIN_DATA_DIR}/hmi_drms_ar'
 
@@ -38,11 +39,13 @@ DATA_DIR_GOES_SXI = f'{MAIN_DATA_DIR}/sxi_availability'
 
 DATA_DIR_ZERO_CROSS = f'{MAIN_DATA_DIR}/zero_cross_data'
 
-DATA_DIR_IMG_RUFFUS_OUTPUT = f'{MAIN_DATA_DIR}/ruffus_output_files'
+DATA_DIR_IMG_RUFFUS_OUTPUT = f'{DATA_DIR_IMG_DATA}/ruffus_output_files'
 
 DATA_DIR_AIA = f'{MAIN_DATA_DIR}/aia_data'
 
 RUFFUS_SQLITE_CHECKPOINT_DIR = f'{HOME_DIR}/ruffus_sqlite_checkpoints'
+
+
 
 ### SXI http path
 
