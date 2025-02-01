8ebe25f4e201f9edecaa700ad7670b19977b7d62
jpadial15 <jorge.r.padial.doble@vanderbilt.edu>
2025-02-01 12:42:45 -0600

    include parametrization functions

--- modules/LASSO_metrics_module.py
+++ modules/LASSO_metrics_module.py
@@ -3,7 +3,116 @@ import numpy as np
 import pandas as pd
 
 
+def norm_RMSE(data):
+    
+    a = ((np.array(data)))
+    
+#     b = np.array([1]*a.shape[0])
+    
+    return(a)
 
+
+
+def norm_pears(data):
+    
+    a = ((np.array(data)))
+    
+    b = np.array([1]*a.shape[0])
+    
+    normed_pears = np.abs(a-b)/2
+    
+    return(normed_pears)
+
+
+
+def norm_etot(data):
+    
+    a = ((np.array(data)))
+    
+#     b = np.array([1]*a.shape[0])
+
+
+    def custom_function(x):
+        
+        
+        if x > 0 and x <1: # overestimating the vector ==> from 1 (worst) to zero (best)
+            return(1-x)
+        
+        if x < 0 and x > -1: # opposite coef ==> capped at 1
+            return(1)
+        
+        if x > 1 and x <2: # underestimating the vector ==> from 0 (best) to 1 (worst)
+            return(x-1)
+        
+        if x > 2:
+            return(1)
+        
+        if x < -1:
+            return(1+x-x)
+
+    
+    y_values = [custom_function(a) for a in data]
+    
+    return(y_values)
+
+def norm_vtot(data):
+    
+    a = ((np.array(data)))
+#     b = np.array([1]*a.shape[0])
+
+    def custom_function(x):
+        
+        
+        if x > 0 and x <1:
+            return(np.sqrt(np.log(2-x))) # from zero to .69
+        
+        if x < 0 and x > -1:
+            return(1)
+        
+        if x > 1 and x <2:
+            return((np.sqrt(1-np.exp(1-x)))) # from zero to .63
+        
+        if x > 2:
+            return(1)
+        
+        if x < -1:
+            return(1+x-x)
+
+    
+    y_values = [custom_function(a) for a in data]
+
+    return(y_values)
+
+
+def test_norm_vtot_etot(data):
+    
+    a = ((np.array(data)))
+    b = np.array([1]*a.shape[0])
+
+    def custom_function(x):
+        
+        
+        if x > 0 and x < 1:
+            return(1-x)
+
+        if x < 0 and x > -1:
+            return(1-x)
+        
+        if x > 1 and x <2:
+#             
+            return(x-1)
+        
+        if x > 2:
+            return(x-1)
+        
+        if x < -1:
+
+            return(1-x)
+
+    
+    y_values = [custom_function(a) for a in data]
+
+    return(y_values)
 
 
 def best_fit(this_df):
